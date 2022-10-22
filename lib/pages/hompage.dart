91b623bd1a0c7810167f7fc8776ffd0fbcb66edf
Sowndarya Krishnan N <krishnaofficial27@gmail.com>
2022-10-23 00:41:54 +0530

    Implemented gesture detector for snake direction

--- lib/pages/hompage.dart
+++ lib/pages/hompage.dart
@@ -13,29 +13,82 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+//snake direction
+enum snakeDirection { UP, DOWN, LEFT, RIGHT }
+
 class _HomePageState extends State<HomePage> {
   //grid dimensions
 
-  int rowSize = 10, totalNoOfSquares = 0;
+  int rowSize = 10, totalNoOfSquares = 100;
 
   //snake position
   List<int> snakePosition = [0, 1, 2];
 
   //food location
   int foodPosition = 55;
-  static var myFont = GoogleFonts.pressStart2p(textStyle: TextStyle(color: Colors.black,letterSpacing: 3));
+  static var myFont = GoogleFonts.pressStart2p(
+      textStyle: TextStyle(color: Colors.black, letterSpacing: 3));
+
+  //snake direction is intially to the right
+  var currentDirection = snakeDirection.RIGHT;
 
   //startGame
-  void startGame(){
+  void startGame() {
     Timer.periodic(Duration(milliseconds: 200), (timer) {
       setState(() {
-        //add a new head and remove the tail
-
-
-
+        moveSnake();
       });
     });
   }
+
+  void moveSnake() {
+    switch (currentDirection) {
+      case snakeDirection.RIGHT:
+        {
+          if (snakePosition.last % rowSize == 9) {
+            snakePosition.add(snakePosition.last + 1 - rowSize);
+          } else {
+            snakePosition.add(snakePosition.last + 1);
+          }
+
+          snakePosition.removeAt(0);
+        }
+        break;
+      case snakeDirection.LEFT:
+        {
+          if (snakePosition.last % rowSize == 0) {
+            snakePosition.add(snakePosition.last - 1 + rowSize);
+          } else {
+            snakePosition.add(snakePosition.last - 1);
+          }
+
+          snakePosition.removeAt(0);
+        }
+        break;
+      case snakeDirection.UP:
+        {
+          if (snakePosition.last < rowSize) {
+            snakePosition.add(snakePosition.last - rowSize + totalNoOfSquares);
+          } else {
+            snakePosition.add(snakePosition.last - rowSize);
+          }
+          snakePosition.removeAt(0);
+        }
+        break;
+      case snakeDirection.DOWN:
+        {
+          if (snakePosition.last + rowSize > totalNoOfSquares) {
+            snakePosition.add(snakePosition.last + rowSize - totalNoOfSquares);
+          } else {
+            snakePosition.add(snakePosition.last + rowSize);
+          }
+          snakePosition.removeAt(0);
+        }
+        break;
+      default:
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,35 +99,63 @@ class _HomePageState extends State<HomePage> {
           Expanded(child: Container()),
           Expanded(
               flex: 3,
-              child: GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 100,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 10),
-                  itemBuilder: (context, index) {
-                   if(snakePosition.contains(index)){
-                     return SnakePixel();
-                   }
-                   else if(foodPosition == index){
-                     return FoodPixel();
-                   }
-                   else{
-                     return const BlankPixel();
-                   }
-                  })),
-          Expanded(child: GestureDetector(
-            onTap: (){
-
+              child: GestureDetector(
+                onVerticalDragUpdate: (details) {
+                  if (details.delta.dy > 0 &&
+                      currentDirection != snakeDirection.UP) {
+                    // print('move up');
+                    currentDirection = snakeDirection.DOWN;
+                  } else if (details.delta.dy < 0 &&
+                      currentDirection != snakeDirection.DOWN) {
+                    // print('move down');
+                    currentDirection = snakeDirection.UP;
+                  }
+                },
+                onHorizontalDragUpdate: (details) {
+                  if (details.delta.dx > 0 &&
+                      currentDirection != snakeDirection.LEFT) {
+                    // print('move right');
+                    currentDirection = snakeDirection.RIGHT;
+                  } else if (details.delta.dx < 0 &&
+                      currentDirection != snakeDirection.RIGHT) {
+                    // print('move left');
+                    currentDirection = snakeDirection.LEFT;
+                  }
+                },
+                child: GridView.builder(
+                    itemCount: 100,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 10),
+                    itemBuilder: (context, index) {
+                      if (snakePosition.contains(index)) {
+                        return SnakePixel();
+                      } else if (foodPosition == index) {
+                        return FoodPixel();
+                      } else {
+                        return const BlankPixel();
+                      }
+                    }),
+              )),
+          Expanded(
+              child: GestureDetector(
+            onTap: () {
+              startGame();
             },
             child: Padding(
-              padding: EdgeInsets.only(left: 70,right: 70,bottom: 80),
+              padding: EdgeInsets.only(left: 70, right: 70, bottom: 80),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: Container(
                   padding: EdgeInsets.all(30),
                   color: Colors.grey,
                   child: Center(
-                    child: Text('PLAY GAME',style: myFont.copyWith(fontSize: 10,),),
+                    child: Text(
+                      'PLAY GAME',
+                      style: myFont.copyWith(
+                        fontSize: 10,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -83,7 +164,6 @@ class _HomePageState extends State<HomePage> {
         ],
         //grid
         //playbutton
-
       ),
     );
   }
