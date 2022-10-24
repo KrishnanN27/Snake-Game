8ae35aa1206635020d3cfe19431a38ed3ca97c56
Sowndarya Krishnan N <krishnaofficial27@gmail.com>
2022-10-24 11:07:19 +0530

    Fetched the highscore details from firebase

---
+++ lib/pages/highscore_tile.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HighScoreTile extends StatelessWidget {
+  final String documentId;
+
+  const HighScoreTile({Key? key,required this.documentId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+     var myFont = GoogleFonts.pressStart2p(
+        textStyle: TextStyle(color: Colors.black, letterSpacing: 3));
+
+    //get the collection of highscores
+    CollectionReference highScores = FirebaseFirestore.instance.collection('highscores');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: highScores.doc(documentId).get(),
+        builder: (context,snapshot){
+        if(snapshot.connectionState == ConnectionState.done){
+          Map<String,dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+          
+          return Column(
+            children: [
+
+              SizedBox(height: 8,),
+
+              Row(
+                children: [
+                  Text(data['name'],style: myFont.copyWith(color: Colors.white,fontSize: 10),),
+                  SizedBox(width: 10,),
+                  Text(data['score'].toString(),style: myFont.copyWith(color: Colors.white,fontSize: 10),),
+
+                ],),
+            ],
+          );
+        }
+        else{
+          return Text('loading',style: myFont.copyWith(color: Colors.white,fontSize: 10),);
+        }
+        });
+  }
+}
--- lib/pages/hompage.dart
+++ lib/pages/hompage.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:snake/pages/blank_pixel.dart';
 import 'package:snake/pages/food_pixel.dart';
+import 'package:snake/pages/highscore_tile.dart';
 import 'package:snake/pages/snake_pixel.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,6 +25,7 @@ class _HomePageState extends State<HomePage> {
   int currentScore = 0;
 
   bool gameHasStarted = false;
+  final nameController = TextEditingController();
 
   //grid dimensions
 
@@ -32,6 +36,11 @@ class _HomePageState extends State<HomePage> {
 
   //food location
   int foodPosition = 55;
+
+  //highscore list
+  List<String> highscoreListId = [];
+  late final Future? letGetDocIds;
+
   static var myFont = GoogleFonts.pressStart2p(
       textStyle: TextStyle(color: Colors.black, letterSpacing: 3));
 
@@ -41,11 +50,29 @@ class _HomePageState extends State<HomePage> {
   //game over
   bool gameOver() {
     // the game is over when the snake runs into itself
-    List<int> bodySnake = snakePosition.sublist(0, snakePosition.length -1);
-    if (bodySnake.contains(snakePosition.last )) {
+    List<int> bodySnake = snakePosition.sublist(0, snakePosition.length - 1);
+    if (bodySnake.contains(snakePosition.last)) {
       return true;
     }
     return false;
+  }
+
+  @override
+  void initState() {
+    letGetDocIds = getDocId();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance
+        .collection("highscores")
+        .orderBy('score', descending: true)
+        .limit(5)
+        .get()
+        .then((value) => value.docs.forEach((element) {
+              highscoreListId.add(element.reference.id);
+            }));
   }
 
   //startGame
@@ -68,6 +95,7 @@ class _HomePageState extends State<HomePage> {
                     children: [
                       Text('Your score is:' + currentScore.toString()),
                       TextField(
+                        controller: nameController,
                         decoration: InputDecoration(hintText: "Enter Name"),
                       )
                     ],
@@ -90,7 +118,9 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void newGame() {
+  Future newGame() async{
+    highscoreListId = [];
+    await getDocId();
     setState(() {
       snakePosition = [0, 1, 2];
       foodPosition = 55;
@@ -100,7 +130,16 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void submitScore() {}
+  void submitScore() {
+    //get access to collectionm
+    var database = FirebaseFirestore.instance;
+
+    //add data to firebase
+    database.collection('highscores').add({
+      "name": nameController.text,
+      "score": currentScore,
+    });
+  }
 
   void eatFood() {
     currentScore++;
@@ -162,7 +201,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     //getting the screen width
 
     double screenWidth = MediaQuery.of(context).size.width;
@@ -170,111 +208,130 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: SizedBox(
-        width: screenWidth > 428  ? 428 : screenWidth,
-        child: Column(
-          //highscores
-          children: [
-            Expanded(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //user current score
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Current Score',
-                      style: myFont.copyWith(color: Colors.grey, fontSize: 10),
+        width: screenWidth > 428 ? 428 : screenWidth,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            //highscores
+            children: [
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //user current score
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Current Score',
+                          style:
+                              myFont.copyWith(color: Colors.grey, fontSize: 10),
+                        ),
+                        SizedBox(
+                          height: 18,
+                        ),
+                        Text(
+                          currentScore.toString(),
+                          style: myFont.copyWith(fontSize: 24),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Text(
-                      currentScore.toString(),
-                      style: myFont.copyWith(fontSize: 24),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'High Scores',
-                      style: myFont.copyWith(color: Colors.grey, fontSize: 10),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(width: 12,),
+                  Expanded(
+                    child:
+                        Padding(
+                          padding: const EdgeInsets.all(13.0),
+                          child: gameHasStarted
+                              ? Container()
+                              : FutureBuilder(
+                              future: letGetDocIds,
+                              builder: (context, snapshot) {
+                                return ListView.builder(
+                                    itemCount: highscoreListId.length,
+                                    itemBuilder: (context, index) {
+                                      return HighScoreTile(
+                                          documentId: highscoreListId[index]);
+                                    });
+                              }),
+                        ),
 
-                //highscore,top 5
-              ],
-            )),
-            Expanded(
-                flex: 3,
-                child: GestureDetector(
-                  onVerticalDragUpdate: (details) {
-                    if (details.delta.dy > 0 &&
-                        currentDirection != snakeDirection.UP) {
-                      // print('move up');
-                      currentDirection = snakeDirection.DOWN;
-                    } else if (details.delta.dy < 0 &&
-                        currentDirection != snakeDirection.DOWN) {
-                      // print('move down');
-                      currentDirection = snakeDirection.UP;
-                    }
-                  },
-                  onHorizontalDragUpdate: (details) {
-                    if (details.delta.dx > 0 &&
-                        currentDirection != snakeDirection.LEFT) {
-                      // print('move right');
-                      currentDirection = snakeDirection.RIGHT;
-                    } else if (details.delta.dx < 0 &&
-                        currentDirection != snakeDirection.RIGHT) {
-                      // print('move left');
-                      currentDirection = snakeDirection.LEFT;
-                    }
-                  },
-                  child: GridView.builder(
-                      itemCount: 100,
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 10),
-                      itemBuilder: (context, index) {
-                        if (snakePosition.contains(index)) {
-                          return SnakePixel();
-                        } else if (foodPosition == index) {
-                          return FoodPixel();
-                        } else {
-                          return const BlankPixel();
-                        }
-                      }),
-                )),
-            Expanded(
-                child: GestureDetector(
-              onTap: () {
-                gameHasStarted ? () {} : startGame();
-              },
-              child: Padding(
-                padding: EdgeInsets.only(left: 70, right: 70, bottom: 80),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    padding: EdgeInsets.all(30),
-                    color: gameHasStarted ? Colors.grey[700] : Colors.grey,
-                    child: Center(
-                      child: Text(
-                        'PLAY GAME',
-                        style: myFont.copyWith(
-                          fontSize: 10,
+                  ),
+                  //high score,top 5
+
+                ],
+              )),
+              Expanded(
+                  flex: 3,
+                  child: GestureDetector(
+                    onVerticalDragUpdate: (details) {
+                      if (details.delta.dy > 0 &&
+                          currentDirection != snakeDirection.UP) {
+                        // print('move up');
+                        currentDirection = snakeDirection.DOWN;
+                      } else if (details.delta.dy < 0 &&
+                          currentDirection != snakeDirection.DOWN) {
+                        // print('move down');
+                        currentDirection = snakeDirection.UP;
+                      }
+                    },
+                    onHorizontalDragUpdate: (details) {
+                      if (details.delta.dx > 0 &&
+                          currentDirection != snakeDirection.LEFT) {
+                        // print('move right');
+                        currentDirection = snakeDirection.RIGHT;
+                      } else if (details.delta.dx < 0 &&
+                          currentDirection != snakeDirection.RIGHT) {
+                        // print('move left');
+                        currentDirection = snakeDirection.LEFT;
+                      }
+                    },
+                    child: GridView.builder(
+                        itemCount: 100,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 10),
+                        itemBuilder: (context, index) {
+                          if (snakePosition.contains(index)) {
+                            return SnakePixel();
+                          } else if (foodPosition == index) {
+                            return FoodPixel();
+                          } else {
+                            return const BlankPixel();
+                          }
+                        }),
+                  )),
+              Text('@CREATEDBYKRISHNA',style: myFont,),
+              SizedBox(height: 29,),
+              Expanded(
+                  child: GestureDetector(
+                onTap: () {
+                  gameHasStarted ? () {} : startGame();
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(left: 70, right: 70, bottom: 80),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding: EdgeInsets.all(30),
+                      color: gameHasStarted ? Colors.grey[700] : Colors.grey,
+                      child: Center(
+                        child: Text(
+                          'PLAY GAME',
+                          style: myFont.copyWith(
+                            fontSize: 10,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )),
-          ],
-          //grid
-          //playbutton
+              )),
+            ],
+            //grid
+            //playbutton
+          ),
         ),
       ),
     );
